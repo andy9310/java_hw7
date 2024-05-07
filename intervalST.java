00ecfc907a877b4c773915f4d819c27fa8139213
andy9310 <r12921a25@ntu.edu.tw>
2024-05-08 01:21:40 +0800

    [dev] java test jar in window

--- intervalST.java
+++ intervalST.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class IntervalST<Key extends Comparable<Key>, Value>{
     private Node root;
 
@@ -12,7 +13,7 @@ class IntervalST<Key extends Comparable<Key>, Value>{
             this.lo = lo;
             this.hi = hi;
             this.val = val;
-            this.max = -1;
+            this.max = this.hi;
         }
     }
 
@@ -24,6 +25,7 @@ class IntervalST<Key extends Comparable<Key>, Value>{
     public void put(Key lo, Key hi, Value val)
     {
          root = putting(root, lo, hi, val);
+         // check_right_node(root);
         // insert a new interval here.
         // lo    : the starting point of the interval. lo included
         // hi    : the ending point of the interval. hi included
@@ -44,6 +46,10 @@ class IntervalST<Key extends Comparable<Key>, Value>{
             }
         }
         x.max = max3(x.hi, max(x.left), max(x.right));
+        if(x.max == null){
+            System.out.println("wrong");
+        }
+        
         return x;
     }
     private Key max(Key a, Key b) {
@@ -51,6 +57,13 @@ class IntervalST<Key extends Comparable<Key>, Value>{
         if (b == null) return a;
         return (a.compareTo(b) > 0) ? a : b;
     }
+    ////
+    private Key max(Node x) {
+        if (x == null) return null;
+        if (x.right == null) return x.hi;
+        else return max(x.right);
+    }
+
     private Key max3(Key a, Key b, Key c) {
         return max(max(a, b), c);
     }
@@ -59,6 +72,7 @@ class IntervalST<Key extends Comparable<Key>, Value>{
     public void delete(Key lo, Key hi)
     {
         root = deleting(root, lo, hi);
+        // check_right_node(root);
         // remove an interval of [lo,hi]
         // do nothing if interval not found.
     }
@@ -68,16 +82,24 @@ class IntervalST<Key extends Comparable<Key>, Value>{
         int cmp = lo.compareTo(x.lo);
         if (cmp < 0) x.left = deleting(x.left, lo, hi);
         else if (cmp > 0) x.right = deleting(x.right, lo, hi);
-        else if (x.hi.equals(hi)) {
-            if (x.right == null) return x.left;
-            if (x.left == null) return x.right;
-            Node t = x;
-            x = min(t.right);
-            x.right = deleteMin(t.right);
-            x.left = t.left;
+        else{
+            int end_cmp = hi.compareTo(x.hi);
+            if(end_cmp < 0) x.left = deleting(x.left, lo, hi);
+            else if (end_cmp > 0) x.right = deleting(x.right, lo, hi);
+            else{
+                if (x.right == null) return x.left;
+                if (x.left == null) return x.right;
+                Node t = x;
+                x = min(t.right);
+                x.right = deleteMin(t.right);
+                x.left = t.left;
+            }  
         }
 
         x.max = max3(x.hi, max(x.left), max(x.right));
+        if(x.max == null){
+            System.out.println("delete wrong");
+        }
         return x;
     }
     private Node min(Node x) {
@@ -88,6 +110,9 @@ class IntervalST<Key extends Comparable<Key>, Value>{
         if (x.left == null) return x.right;
         x.left = deleteMin(x.left);
         x.max = max3(x.hi, max(x.left), max(x.right));
+        if(x.max == null){
+            System.out.println("deleteMin wrong");
+        }
         return x;
     }
 
@@ -99,18 +124,27 @@ class IntervalST<Key extends Comparable<Key>, Value>{
         // return the values of all intervals within the tree which intersect with [lo,hi].
     }
     private void intersects(Node x, Key lo, Key hi, List<Value> result) {
-        if (x == null) return;
+        if (x == null){
+            System.out.println("null");
+            return;
+        }
         if (intersects(x.lo, x.hi, lo, hi)) result.add(x.val);
 
         if (x.left != null && x.left.max.compareTo(lo) >= 0)
             intersects(x.left, lo, hi, result);
-        if (x.right != null && x.hi.compareTo(lo) >= 0)
+        if (x.right != null && x.right.max.compareTo(lo) >= 0){
             intersects(x.right, lo, hi, result);
+        }
+            
     }
     private boolean intersects(Key lo1, Key hi1, Key lo2, Key hi2) {
         return lo1.compareTo(hi2) <= 0 && hi1.compareTo(lo2) >= 0;
     }
-    
+    private void check_right_node(Node x){
+        if(x == null) return;
+        System.out.println(x.val);
+        check_right_node(x.right);
+    }
 
 
 
